f0a165abf2a0883565c5e7a728b36c48a6a08e55
yaswanth70 <131728383+yaswanth70@users.noreply.github.com>
2024-04-22 11:33:17 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "192.170.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-1b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -31,7 +31,7 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "public_subnet_2" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "192.170.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-1b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -75,6 +75,7 @@ resource "aws_route_table_association" "public_rta_2" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
 
 # =========================
 # Create your subnets here
