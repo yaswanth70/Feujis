3d2a759a8f26d858596214b92255b7158845d879
yaswanth70 <131728383+yaswanth70@users.noreply.github.com>
2024-04-25 12:06:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,31 +1,24 @@
+# variables.tf
+
 variable "aws_region" {
-  description = "AWS region to use"
-  type        = string
-  default     = "us-west-1"
-}
-
-variable "aws_access_key" {
-  description = "AWS Access Key ID for the target AWS account"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key for the target AWS account"
-  type        = string
-}
-
-variable "aws_session_token" {
-  description = "AWS Session Token for the target AWS account. Required only if authenticating using temporary credentials"
-  type        = string
-  default     = ""
+  description = "AWS region where the resources will be provisioned"
+  default     = "us-west-2" # Change this to your desired region
 }
 
 variable "vpc_cidr" {
-  description = "The IPv4 CIDR block to use for the VPC"
-  type        = string
-  default     = "192.170.0.0/20"
-  validation {
-    condition     = tonumber(split("/", var.vpc_cidr)[1]) <= 20 && tonumber(split("/", var.vpc_cidr)[1]) >= 16
-    error_message = "CIDR size must be at least /20 and no larger than /16"
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16" # Change this to your desired VPC CIDR block
+}
+
+variable "vpc_name" {
+  description = "Name for the VPC"
+  default     = "my-vpc" # Change this to your desired VPC name
+}
+
+variable "tags" {
+  description = "Tags to apply to AWS resources"
+  type        = map(string)
+  default     = {
+    Environment = "Production"
   }
 }
