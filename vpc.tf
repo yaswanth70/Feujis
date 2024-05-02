3e7b8d312c2e2c9f255e12817d7920192ccc88ce
yaswanth70 <131728383+yaswanth70@users.noreply.github.com>
2024-05-02 19:46:24 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -14,12 +14,12 @@ variable "subnet_cidr_prefix" {
   default     = 24
 }
 
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "my_vpc1" {
   cidr_block = var.vpc_cidr_block
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
+  vpc_id            = aws_vpc.my_vpc1.id
   cidr_block        = cidrsubnet(var.vpc_cidr_block, var.subnet_cidr_prefix, 0)
   availability_zone = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
@@ -30,7 +30,7 @@ resource "aws_subnet" "public_subnet" {
 }
 
 resource "aws_subnet" "private_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
+  vpc_id            = aws_vpc.my_vpc1.id
   cidr_block        = cidrsubnet(var.vpc_cidr_block, var.subnet_cidr_prefix, 1)
   availability_zone = data.aws_availability_zones.available.names[0]
 
